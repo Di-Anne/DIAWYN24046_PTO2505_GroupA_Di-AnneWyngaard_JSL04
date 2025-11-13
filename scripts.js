cf61423e0d99bcb05d6fe69eb7457e9854529c66
Di-Anne Wyngaard <dianneleighwyngaard@gmail.com>
2025-11-13 13:39:14 +0200

    Create on task div in the DOM

--- scripts.js
+++ scripts.js
@@ -1,6 +1,17 @@
 import { initialTasks } from "./initialData.js";
 
-function createCardElement()
+// Create one task element in the DOM
+function createTaskElement(task) {
+  const taskElement = document.createElement('div');
+  taskElement.className = "task";
+  taskElement.textContent = task.title;
+
+  taskElement.addEventListener("click", () => {
+   openTaskModal(task);
+  });
+
+  return taskElement; 
+}
 
 
 
@@ -8,16 +19,16 @@ function createCardElement()
 
 
 
-
-function openTaskModal() {
+// Show modal and present prefilled-fields 
+function openTaskModal(task) {
   const modal = document.getElementById('task-modal');
   const titleInput = document.getElementById('task-title');
   const descripInput = document.getElementById('task-descrip');
   const statusSelect = document.getElementById('task-status');
 
-  titleInput.value = 
-  descripInput.value =
-  statusSelect.value =
+  titleInput.value = task.title;
+  descripInput.value = task.description;
+  statusSelect.value = task.status;
 
   modal.showModal();
 }
